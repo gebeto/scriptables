a125afbd606e187a9aa9e647d29ab4ef47c6c371
gebeto <slavik.nychkalo@gmail.com>
2021-05-30 18:04:50 +0300

    Added example reference for types

--- widgets/InfoWidget.js
+++ widgets/InfoWidget.js
@@ -1,7 +1,7 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: light-brown; icon-glyph: magic;
-
+///<reference path="../index.d.ts" />
 
 const ww = new ListWidget()
 ww.setPadding(0,0,0,0);
