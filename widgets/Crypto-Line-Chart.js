ef43b8d2c1c22e357a5f7a52c01c57d98ef4420d
gebeto <slavik.nychkalo@gmail.com>
2022-03-31 10:39:13 +0300

    import script

--- widgets/Crypto-Line-Chart.js
+++ widgets/Crypto-Line-Chart.js
@@ -4,11 +4,11 @@
 ///<reference path="../index.d.ts" />
 
 const data_ = [[1], [1.9], [4], [2.8], [5], [2.4]];
-const crypto = args.widgetParameter || "btc"
-const req = new Request("https://api.kuna.io/v3/book/"+crypto+"uah")
-const dd = await req.loadJSON()
-const data = dd.slice(0, 30)
-data.reverse()
+const crypto = args.widgetParameter || "btc";
+const req = new Request("https://api.kuna.io/v3/book/" + crypto + "uah");
+const dd = await req.loadJSON();
+const data = dd.slice(0, 30);
+data.reverse();
 
 const lw = new ListWidget();
 
@@ -19,17 +19,17 @@ const chart = importModule("modules/LineChart").drawChart({
   chartHeight: 140,
   spacing: 50,
 });
-lw.backgroundImage = chart.getImage()
+lw.backgroundImage = chart.getImage();
 const text = lw.addText(crypto.toUpperCase());
-text.url = "https://github.com"
+text.url = "https://github.com";
 text.leftAlignText();
-text.font = Font.boldMonospacedSystemFont(24)
+text.font = Font.boldMonospacedSystemFont(24);
 text.textColor = Color.black();
-const dateText = lw.addDate(new Date())
-dateText.applyTimeStyle()
-dateText.font = Font.boldMonospacedSystemFont(8)
-dateText.textColor = Color.black()
-dateText.textOpacity = 0.3
-lw.addSpacer()
+const dateText = lw.addDate(new Date());
+dateText.applyTimeStyle();
+dateText.font = Font.boldMonospacedSystemFont(8);
+dateText.textColor = Color.black();
+dateText.textOpacity = 0.3;
+lw.addSpacer();
 Script.setWidget(lw);
-lw.presentSmall()
+lw.presentSmall();
